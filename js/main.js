ec2aaaad7148a6139a088cd1050301ace6186f6a
admocreate <admoore.shitta@gmail.com>
2021-04-13 14:50:02 +0100

    fixed customer care page

--- js/main.js
+++ js/main.js
@@ -22,7 +22,11 @@
 
 // Open and Close Button for Search Section
 function openSearch() {
-document.getElementById("myOverlay").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
 }
+function closeSearch() {
+document.getElementById("myOverlay").style.display = "none";
+}
+
 
 
